9deb2e5612ec2b3c238653c2dadd5ab5219e55f9
iUnknownWs <109998892+iUnknownWs@users.noreply.github.com>
2025-09-16 21:24:56 -0400

    feat(tendencias): implement layout and content structure for tendencias page

--- app/pages/tendencias/index.vue
+++ app/pages/tendencias/index.vue
@@ -1,5 +1,67 @@
 <template>
-  <div></div>
+  <div class="min-h-screen p-8">
+    <div class="breadcrumbs pb-6 text-lg">
+      <ul>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li>Tendencias</li>
+      </ul>
+    </div>
+    <div class="flex gap-8">
+      <div class="w-full max-w-4xl">
+        <CardComponent class="mb-6">
+          <NuxtImg
+            src="/hero.jpg"
+            class="w-full rounded-t-2xl"
+            alt="Tendencia Principal"
+            height="400"
+          />
+          <div class="p-6">
+            <h2 class="h3 mb-4">Tendencia Principal</h2>
+            <p class="body">
+              Esta es una descripción detallada de la tendencia principal que
+              está siendo destacada en esta sección. Aquí puedes incluir
+              información relevante y atractiva para los usuarios.
+            </p>
+          </div>
+        </CardComponent>
+        <div class="flex flex-col gap-4">
+          <CardComponent class="flex cursor-pointer">
+            <NuxtImg
+              src="/hero.jpg"
+              class="rounded-l-2xl"
+              alt="Tendencia 1"
+              width="120"
+              height="100"
+            />
+            <div class="flex flex-col gap-1 p-4 text-sm">
+              <p class="h5 link link-hover link-primary">
+                Descripción de la tendencia 1
+              </p>
+              <p class="body">Detalles adicionales sobre la tendencia 1</p>
+            </div>
+          </CardComponent>
+        </div>
+      </div>
+      <div class="flex-1">
+        <div class="flex flex-col gap-4">
+          <p class="h4">Sugeridos</p>
+          <CardComponent class="flex">
+            <NuxtImg
+              src="/hero.jpg"
+              class="rounded-l-2xl"
+              alt="Tendencia 1"
+              width="120"
+              height="100"
+            />
+            <div class="flex flex-col gap-1 p-4 text-sm">
+              <p class="h6">Descripción de la tendencia 1</p>
+              <p class="body">Detalles adicionales sobre la tendencia 1</p>
+            </div>
+          </CardComponent>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup></script>
